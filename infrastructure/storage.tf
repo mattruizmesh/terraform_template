70d9d032138857fe81006d45d158ad24be65d34d
Matt Ruiz <MattRuiz@eaton.com>
2025-02-06 11:21:58 -0500

    pulling storage module from a tagged release

--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -1,10 +1,16 @@
 module "storage_account" {
-  #source = "github.com/mattruizmesh/terraform_module_source/storage"
-  source = "git@github.com-mesh:mattruizmesh/terraform_module_source.git//storage"
+  # Using a tagged release form github
+  source = "github.com/mattruizmesh/terraform_module_source.git//storage?ref=test"
+  
+  # This should work for a public repository
+  # source = "github.com/mattruizmesh/terraform_module_source.git//storage"
+
+  # This is using my local rsa key
+  #source = "git@github.com-mesh:mattruizmesh/terraform_module_source.git//storage"
 
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  storage_account_name = "modulestorage"
+  storage_account_name = "modulestoragerannums129"
   storage_account_tier = "Standard"
   storage_account_replication_type = "LRS"
   tags = {
